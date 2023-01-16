e1e30bbe1ddc50c726971dc83da20d691ec18766
lukaszsereda <lukaszsereda123@gmail.com>
2023-01-16 08:17:13 +0100

    Initial commit

---
+++ ProjektNET/Data/MVCDemoDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProjektNET.Models.Domain;
+
+namespace ProjektNET.Data
+{
+    public class MVCDemoDbContext : DbContext
+    {
+        public MVCDemoDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
